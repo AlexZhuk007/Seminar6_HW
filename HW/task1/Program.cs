358560956112af7a975f378d23d219165a1fff7c
Alex Zhuk <89257358590@mail.ru>
2024-03-09 21:27:49 +0300

    sem6 push

---
+++ HW/task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте двумерный массив символов (тип char [,]). 
+//Создать строку из символов этого массива. 
+
+char[,] matrixchar = { 
+                        {'a','b','c'},
+                        {'d','e','f'}
+                      };
+
+string str = "";
+
+for (int i = 0; i < matrixchar.GetLength(0); i++)
+{
+    for (int j = 0; j < matrixchar.GetLength(1); j++)
+    {
+        str = str + matrixchar[i, j];
+        
+        // Console.Write($"-{matrix[i, j]}-");
+        // Console.Write("\t");
+    }
+    
+}    
+Console.WriteLine(str);
---
+++ HW/task3/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задайте  произвольную  строку.  Выясните, 
+// является  ли  она  палиндромом.
+
+Console.Clear();
+Console.WriteLine("Введите слово для проверки является или оно палиндромом: ");
+string str = Console.ReadLine();
+char[] array = new char[str.Length];
+char[] array2 = new char[str.Length];
+
+for (int i = 0; i < str.Length; i++)
+{
+    array[i] = str[i];
+}
+
+for (int i = 0; i < str.Length; i++)
+{
+    array2[i] = array[i];
+}
+
+Array.Reverse(array2);
+
+string str1 = string.Join(", ", array);
+string str2 = string.Join(", ", array2);
+
+if (str1 == str2)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+// foreach (char item in array2)
+// {
+//     Console.Write(item);
+// }
+
+// if (array==array2)
+// {
+//     Console.WriteLine("Yes");
+// }
+// else
+// {
+//     Console.WriteLine("No");
+// }
+
+// for (int j = 0; j < array.Length / 2; j++)
+// {
+//     if (array[j] == array[array.Length - 1 - j])
+//     {
+//         Console.WriteLine("Да");
+//     }
+
+//     else
+//     {
+//         Console.WriteLine("Нет");
+//     }
+// }
